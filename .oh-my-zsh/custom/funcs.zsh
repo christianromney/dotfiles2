037a7de13a37a8ff11cd96fbbe2a1d1062234709
Christian Romney <cromney@pointslope.com>
2013-04-16 16:44:24 -0400

    Symlinking doesn't add the file contents
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- .oh-my-zsh/custom/funcs.zsh
+++ .oh-my-zsh/custom/funcs.zsh
@@ -1,1 +1,9 @@
-/Users/christian/.oh-my-zsh/custom/funcs.zsh
+source_javarc() {
+  if [ "$PWD" != "$HOME" ]; then
+    if [ -f "$PWD/.javarc" ]; then
+      source "$PWD/.javarc"
+      printenv | grep -i java_home
+    fi
+  fi
+}
+
