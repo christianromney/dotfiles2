105d6e03ad072acb1d17f3b1558ee1d9608ba8bf
Christian Romney <cromney@pointslope.com>
2013-04-29 18:08:09 -0400

    Added emacs-live config
    Signed-off-by: Christian Romney <cromney@pointslope.com>

---
+++ .emacs-live.el
@@ -0,0 +1,17 @@
+; From http://paradigmx.net/blog/2013/04/01/clojure-toolchain-reloaded/
+; Use live-use-packs to control which built-in packs should be loaded
+; The built-in live/colour-pack MUST be disabled if you want to use 3rd party color theme pack
+(live-use-packs '(live/foundation-pack
+                  ;live/colour-pack
+                  live/git-pack
+                  live/clojure-pack
+                  live/lang-pack
+                  live/power-pack
+                  live/bindings-pack))
+
+(live-add-packs '(~/.live-packs/solarized-pack
+                  ~/.live-packs/install-packages-pack
+                  ~/.live-packs/chat-pack
+                  ~/.live-packs/twitter-pack
+                  ~/.live-packs/romney-pack
+                  ))
