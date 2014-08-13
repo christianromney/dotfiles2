0cda5a1b7454c925ee8e163fbc2e855d965453d2
Christian Romney <cromney@pointslope.com>
2014-08-13 01:32:39 -0400

    Add leiningen profiles
    Signed-off-by: Christian Romney <cromney@pointslope.com>

---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[cider/cider-nrepl "0.7.0"]
+                  [lein-cljsbuild "1.0.0-alpha2"]
+                  [lein-immutant "1.1.0"]
+                  [lein-difftest "2.0.0"]
+                  [lein-midje "3.0.0"]
+                  [lein-vanity "0.2.0"]
+                  [lein-try "0.4.1"]
+                  [quickie "0.2.5"]]}}
