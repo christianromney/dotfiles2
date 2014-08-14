691c690736fe60e3273d819f9571daa0994a5697
Christian Romney <cromney@pointslope.com>
2014-08-14 11:12:30 -0400

    Merge homebrew capabilities into sync
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- sync.sh
+++ sync.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+brew list | diff -u -- brews.txt - | sed '1,5D' | awk '/^\-/' | sed 's/\-//' | while read b; do
+  if [ -n "$b" ]; then
+    brew install $b
+  fi
+done
+
+brew cask list | diff -u -- casks.txt - | sed '1,5D' | awk '/^\-/' | sed 's/\-//' | while read c; do
+  if [ -n "$c" ]; then
+    brew cask install $c
+  fi
+done
+
 for d in $(pwd)/*; do
   if [ -d $d ]; then
     stow -t $HOME -R $(basename $d)
