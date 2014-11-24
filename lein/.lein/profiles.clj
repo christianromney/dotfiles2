90cbb0950adb190cb35284fc5619d15d4fb15c76
Christian Romney <cromney@pointslope.com>
2014-11-24 12:13:51 -0500

    Added humane test output, prism, refactor-nrepl
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,6 +1,13 @@
 ;; ~/.lein/profiles.clj
 {:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                  [refactor-nrepl "0.1.0"]
                   [lein-pprint "1.1.2"]
                   [lein-difftest "2.0.0"]
                   [lein-try "0.4.3"]
-                  [jonase/eastwood "0.1.5"]]}}
+                  [lein-typed "0.3.5"]
+                  [jonase/eastwood "0.1.5"]
+                  [com.aphyr/prism "0.1.1"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]
+                       [com.aphyr/prism "0.1.1"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]}}
