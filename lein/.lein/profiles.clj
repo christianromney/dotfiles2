4f5716c6f93d7b79e9d36a000e82c8fe7375e757
Christian Romney <cromney@pointslope.com>
2014-11-26 11:42:04 -0500

    Upgraded to cider 0.8.0 final
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,5 +1,5 @@
 ;; ~/.lein/profiles.clj
-{:user {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.8.0"]
                   [refactor-nrepl "0.1.0"]
                   [lein-pprint "1.1.2"]
                   [lein-difftest "2.0.0"]
