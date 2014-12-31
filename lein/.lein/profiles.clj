2352ef1de5666db2477bd140527f6e0f397f1284
Christian Romney <cromney@pointslope.com>
2014-12-31 09:06:47 -0500

    Upgrade to cider 0.9.0-SNAPSHOT, add test refresh and humane output
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,13 +1,12 @@
 ;; ~/.lein/profiles.clj
-{:user {:plugins [[cider/cider-nrepl "0.8.1"]
-                  [refactor-nrepl "0.1.0"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "0.2.2"]
                   [lein-pprint "1.1.2"]
                   [lein-difftest "2.0.0"]
                   [lein-try "0.4.3"]
                   [lein-typed "0.3.5"]
                   [jonase/eastwood "0.1.5"]
-                  [com.aphyr/prism "0.1.1"]]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]
-                       [com.aphyr/prism "0.1.1"]]
+                  [com.jakemccrary/lein-test-refresh "0.5.2"]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
