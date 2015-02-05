b4e7cdd587fff874a4bc8ca612fa27b1fa6223d2
Christian Romney <cromney@pointslope.com>
2015-02-05 13:23:50 -0500

    Added deft, org-babel for clojure, setup agenda, spellcheck
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- emacs/.emacs.d/personal/prelude-org.el
+++ emacs/.emacs.d/personal/prelude-org.el
@@ -1,41 +1,69 @@
-;; This should be obvious...
-;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(require 'deft)
 
 ;; Export
 (setq org-html-validation-link nil)
 (setq org-export-html-postamble nil)
+(setq org-export-backends '(ascii html icalendar latex md))
 
 ;; File handling
-;; (setq org-directory "~/Dropbox/Notes")
-;; (setq org-default-notes-file (concat org-directory "/notes.org"))
-;; (setq org-log-done 'time)
+(setq org-directory "~/Dropbox/org")
+(setq org-default-notes-file (concat org-directory "/default.org"))
+(setq org-log-done t)
+(setq deft-directory "~/Dropbox/org")
+(setq deft-use-filename-as-title t)
+(setq deft-extension "org")
+(setq deft-text-mode 'org-mode)
+(global-set-key [f8] 'deft)
 
-;; TODO
-(setq org-todo-keywords
-      '((type "NEW" "IN-PROGRESS" "|" "DONE")))
+;; TODOs
+(setq org-todo-keywords '((type "TODO" "IN-PROGRESS" "|" "DONE"))
+      org-todo-keyword-faces '(("INPROGRESS" . (:foreground "blue" :weight bold))))
 
+;;
+(require 'org-trello)
+(custom-set-variables '(org-trello-files '("~/Dropbox/org/trello-scorbot.org")))
+(add-hook 'org-mode-hook 'org-trello-mode)
 
+;; Agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
 
-(setq org-export-backends '(ascii html icalendar latex md))
+(setq org-agenda-show-log t
+      org-agenda-todo-ignore-scheduled t
+      org-agenda-todo-ignore-deadlines t
+      org-agenda-include-diary t)
+
+(setq org-agenda-files (list "~/Dropbox/org/agenda.org"))
+
+;; Run these, too
+(add-hook 'org-mode-hook
+          (lambda ()
+            (flyspell-mode)))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (writegood-mode)))
 
 ;; Babel
 
-;; (setq org-fontify-done-headline t)
-;; (setq org-src-fontify-natively t)
-;; (setq org-confirm-babel-evaluate nil)
-;; (setq org-src-window-setup 'current-window)
+(require 'ob)
+(require 'ob-clojure)
+(require 'cider)
+(setq org-babel-clojure-backend 'cider)
+(setq org-fontify-done-headline t)
+(setq org-src-fontify-natively t)
+(setq org-confirm-babel-evaluate nil)
+(setq org-src-window-setup 'current-window)
 
 
-;; (org-babel-do-load-languages
-;;  'org-babel-load-languages
-;;  '((emacs-lisp . t)
-;;    (sh         . t)
-;;    (clojure    . t)
-;;    (java       . t)
-;;    (js         . t)
-;;    (ruby       . t)
-;;    (python     . t)
-;;    (R          . t)))
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (sh         . t)
+   (clojure    . t)
+   (java       . t)
+   (js         . t)
+   (ruby       . t)
+   (python     . t)))
 
-;; (add-to-list 'org-babel-tangle-lang-exts '("clojure" . "clj"))
-;; (add-to-list 'org-babel-tangle-lang-exts '("js"      . "js"))
+(add-to-list 'org-babel-tangle-lang-exts '("clojure" . "clj"))
+(add-to-list 'org-babel-tangle-lang-exts '("js"      . "js"))
