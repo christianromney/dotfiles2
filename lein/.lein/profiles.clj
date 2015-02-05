911ff69a91a9f436bbc6f6f739980d2c5abd5608
Christian Romney <cromney@pointslope.com>
2015-02-05 13:16:26 -0500

    New cider, ancient, hiera, vanity, annotations
    Signed-off-by: Christian Romney <cromney@pointslope.com>

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,12 +1,28 @@
 ;; ~/.lein/profiles.clj
-{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+{:user {:plugins [;; basic necessities
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  ;; refactoring support
                   [refactor-nrepl "0.2.2"]
+                  ;; pretty print output
                   [lein-pprint "1.1.2"]
+                  ;; shows nicer test output (diff between actual/expected)
                   [lein-difftest "2.0.0"]
+                  ;; try libs before including them for realz
                   [lein-try "0.4.3"]
+                  ;; check for outdated dependencies
+                  [lein-ancient "0.6.1" :exclusions [org.clojure/clojure]]
+                  ;; working with core.typed
                   [lein-typed "0.3.5"]
-                  [jonase/eastwood "0.1.5"]
-                  [com.jakemccrary/lein-test-refresh "0.5.2"]]
+                  ;; linter
+                  [jonase/eastwood "0.1.5" :exclusions [org.clojure/clojure]]
+                  ;; autotest runner
+                  [com.jakemccrary/lein-test-refresh "0.5.2" :exclusions [org.clojure/clojure]]
+                  ;; uses graphviz to make a namespace hierarchy graphA
+                  [lein-hiera "0.9.0"]
+                  ;; lines of code, test code stats
+                  [lein-vanity "0.2.0"]
+                  ;; parses, prints TODOs
+                  [lein-annotations "0.1.0"]]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]}}
