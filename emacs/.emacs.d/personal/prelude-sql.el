a6cb899ab3914bdf30ed59dd9c49c43619d04344
Christian Romney <cromney@pointslope.com>
2016-02-26 10:26:04 -0500

    Make it easy to use SQL client
    The envvars will probably change to reflect the emacs
    var names.
    
    Signed-off-by: Christian Romney <cromney@pointslope.com>

---
+++ emacs/.emacs.d/personal/prelude-sql.el
@@ -0,0 +1,15 @@
+(require 'sql)
+
+(defun personal/mysql-connect ()
+  (interactive)
+  (let ((sql-user (getenv "DB_USER"))
+        (sql-password (getenv "DB_PASS"))
+        (sql-database (getenv "DB_BASE"))
+        (sql-server (getenv "DB_HOST"))
+        (sql-port 3306))
+    (sql-mysql (current-buffer))))
+
+(global-set-key (kbd "<f10>") 'personal/mysql-connect)
+
+
+(provide 'personal/prelude-sql)
