437c298a015d6c7328adab9f5bf22015a08bb416
Christian Romney <christian.a.romney@gmail.com>
2019-03-21 16:21:12 -0400

    Change time-machine bindings, add smerge hydra, git modes
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

--- emacs/.emacs.d/config/romney-coding.el
+++ emacs/.emacs.d/config/romney-coding.el
@@ -36,7 +36,107 @@
 
 (use-package git-timemachine
   :ensure t
-  :bind ("C-c g" . git-timemachine-toggle))
+  :custom-face
+  (git-timemachine-minibuffer-author-face
+   ((t (:inherit font-lock-string-face))))
+  (git-timemachine-minibuffer-detail-face
+   ((t (:inherit warning))))
+  :bind (:map vc-prefix-map
+              ("t" . git-timemachine)))
+
+(use-package git-messenger
+  :ensure t
+  :defer t
+  :bind (:map vc-prefix-map
+              ("p" . git-messenger:popup-message)
+              :map git-messenger-map
+              ("m" . git-messenger:copy-message))
+  :init
+  ;; Use magit-show-commit for showing status/diff commands
+  (setq git-messenger:use-magit-popup t
+        git-messenger:show-detail t))
+
+(use-package smerge-mode
+  :ensure nil
+  :diminish
+  :commands (smerge-mode
+             smerge-auto-leave
+             smerge-next
+             smerge-prev
+             smerge-keep-base
+             smerge-keep-upper
+             smerge-keep-lower
+             smerge-keep-all
+             smerge-keep-current
+             smerge-keep-current
+             smerge-diff-base-upper
+             smerge-diff-upper-lower
+             smerge-diff-base-lower
+             smerge-refine
+             smerge-ediff
+             smerge-combine-with-next
+             smerge-resolve
+             smerge-kill-current)
+  :preface
+  (defhydra smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook ((find-file . (lambda ()
+                        (save-excursion
+                          (goto-char (point-min))
+                          (when (re-search-forward "^<<<<<<< " nil t)
+                            (smerge-mode 1)))))
+         (magit-diff-visit-file . (lambda ()
+                                    (when smerge-mode
+                                      (smerge-hydra/body))))))
+
+(use-package browse-at-remote
+  :ensure t
+  :defer t
+  :bind (:map vc-prefix-map
+              ("B" . browse-at-remote)))
+
+(use-package gitattributes-mode
+  :ensure t
+  :defer t)
+
+(use-package gitconfig-mode
+  :ensure t
+  :defer t)
+
+(use-package gitignore-mode
+  :ensure t
+  :defer t)
 
 (use-package projectile
   :ensure t
