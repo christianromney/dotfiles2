b5525b10d37bd1dad226eb9dc0db98a9bfaf1c48
Christian Romney <christian.a.romney@gmail.com>
2019-03-26 21:43:52 -0400

    Enable unicode input method
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

--- emacs/.emacs.d/config/romney-racket.el
+++ emacs/.emacs.d/config/romney-racket.el
@@ -7,7 +7,10 @@
   :defer t
   :mode ("\\.pie\\'" . racket-mode)
   :bind (:map racket-mode-map
-         ("C-c r". racket-run)))
+              ("C-c r". racket-run))
+  :config
+  (add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
+  (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable))
 
 (provide 'romney-racket)
 ;;; romney-racket.el ends here
