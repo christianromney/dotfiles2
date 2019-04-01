e3d21b7654bb52abd15e0fecc03baf9b8b64559e
Christian Romney <christian.a.romney@gmail.com>
2019-04-01 13:03:30 -0400

    Fixed org-re-reveal configuration
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

---
+++ emacs/.emacs.d/config/romney-coding-langs.el
@@ -0,0 +1,11 @@
+;;; romney-coding-langs.el --- Misc programming languages
+;;; Commentary:
+;;; Secondary language modes mainly for learning
+;;; Code:
+(use-package mips-mode
+  :ensure t
+  :defer t
+  :mode "\\.mips$")
+
+(provide 'romney-coding-langs)
+;;; romney-coding-langs.el ends here
