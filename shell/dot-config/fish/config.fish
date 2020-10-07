d751072b85a49fd66b1836aa3972ad43a71396cc
Christian Romney <christian.a.romney@gmail.com>
2020-10-06 20:07:40 -0400

    Use starship for prompt
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

--- shell/dot-config/fish/config.fish
+++ shell/dot-config/fish/config.fish
@@ -8,17 +8,13 @@ end
 
 set EDITOR emacs
 
-# prompt configuration
-set SPACEFISH_PROMPT_ORDER time user host dir git package conda node docker ruby rust julia aws exec_time line_sep exit_code jobs char
-set SPACEFISH_TIME_SHOW true
-set SPACEFISH_TIME_SUFFIX " $SPACEFISH_PROMPT_DEFAULT_SUFFIX"
-set SPACEFISH_USER_SHOW "needed"
-
 # Path
 set PATH $HOME/.cargo/bin $HOME/bin $HOME/.emacs.d/bin $PATH
 
 # Python, Ruby, Rust, Direnv
 status --is-interactive; and source (pyenv init -|psub)             # python
 status --is-interactive; and source (rbenv init -|psub)             # ruby
-set RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src  # rust
+
 eval (direnv hook fish)                                             # direnv
+
+starship init fish | source
