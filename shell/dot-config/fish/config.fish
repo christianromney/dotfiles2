7681747df452db33819debe2280f2d4e9dfac265
Christian Romney <christian.romney@cognitect.com>
2020-10-23 16:53:31 -0400

    Remove redundant exporting of SSH keys
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- shell/dot-config/fish/config.fish
+++ shell/dot-config/fish/config.fish
@@ -1,11 +1,4 @@
-set fish_greeting \n"|->>> Welcome back to "(hostname -s)", "(whoami)". <<<-|"\n
-
-# add keys to ssh-agent
-set keys "rsa" "ed25519"
-for k in "$HOME/.ssh/id_"$keys
-  ssh-add -K $k > /dev/null 2>&1
-end
-
+set fish_greeting \n"|->>> Welcome back, "(whoami)". <<<-|"\n
 set EDITOR emacs
 
 # Path
