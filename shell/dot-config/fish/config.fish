43d2877828fc4deb3ed424bf01d26b5722f62c20
Christian Romney <christian.romney@cognitect.com>
2020-11-05 17:35:42 -0500

    Source .nurc, add jenv, ignore lein
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- shell/dot-config/fish/config.fish
+++ shell/dot-config/fish/config.fish
@@ -1,13 +1,19 @@
-set fish_greeting \n"|->>> Welcome back, "(whoami)". <<<-|"\n
+set fish_greeting ">>> Welcome back, "(whoami)". <<<"
 set EDITOR emacs
 
 # Path
-set PATH $HOME/.cargo/bin $HOME/bin $HOME/.emacs.d/bin $PATH
+set PATH $HOME/.cargo/bin $HOME/bin $HOME/.emacs.d/bin $HOME/.jenv/bin  $PATH
+
+# Nubank
+if test -f $HOME/.nurc
+  source $HOME/.nurc
+end
 
 # Python, Ruby, Rust, Direnv
-status --is-interactive; and source (pyenv init -|psub)             # python
-status --is-interactive; and source (rbenv init -|psub)             # ruby
+status --is-interactive; and source (pyenv init -|psub) # python
+status --is-interactive; and source (rbenv init -|psub) # ruby
+status --is-interactive; and source (jenv init -|psub)  # jenv
 
-eval (direnv hook fish)                                             # direnv
+eval (direnv hook fish)                                 # direnv
 
-starship init fish | source
+starship init fish | source                             # prompt
