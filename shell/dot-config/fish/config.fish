2e523ded72c10771a9d1def17403b9922e85810f
Christian Romney <christian.romney@cognitect.com>
2020-12-18 09:52:38 -0500

    Explicitly set gpg program
    Also requires setting pinentry-program /usr/local/bin/pinentry-mac
    in $HOME/.gnupg/gpg-agent.conf and killall gpg-agent
    
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- shell/dot-config/fish/config.fish
+++ shell/dot-config/fish/config.fish
@@ -1,6 +1,7 @@
 set fish_greeting ">>> Welcome back, "(whoami)". <<<"
 set EDITOR emacs
-set PATH $HOME/bin /usr/local/MacGPG2/bin $HOME/.cargo/bin $HOME/.emacs.d/bin $HOME/.jenv/bin $PATH
+set PATH /usr/local/MacGPG2/bin $HOME/bin $HOME/.cargo/bin $HOME/.emacs.d/bin $HOME/.jenv/bin $PATH
+set -x GPG_TTY (tty)
 
 if test -f $HOME/.nurc
   bass source $HOME/.nurc
