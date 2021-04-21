adbbc38755dbf6ca6009de19545b51e882e6eedb
Christian Romney <christian.romney@cognitect.com>
2021-04-21 09:19:28 -0400

    Added C/C++ support
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- doom/dot-doom.d/init.el
+++ doom/dot-doom.d/init.el
@@ -120,8 +120,8 @@
 
        :lang
        ;;agda              ; types of types of types of types...
-       ;;cc                ; C/C++/Obj-C madness
-       ;;clojure           ; java with a lisp
+       (cc +lsp)           ; C/C++/Obj-C madness
+       ;; clojure          ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
