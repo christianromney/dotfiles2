f3128732eba7b228e968de4d03205f2e043159cc
Christian Romney <christian.romney@cognitect.com>
2022-03-07 13:09:30 -0500

    Upgrade fish plugins
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- shell/dot-config/fish/functions/man.fish
+++ shell/dot-config/fish/functions/man.fish
@@ -1,21 +1,21 @@
-#!/usr/bin/env fish
-
-# colors documentation can be found at
-# http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html#256-colors
 function man --wraps man -d "Run man with added colors"
-    set -l bold_ansi_code "\u001b[1m"
-    set -l underline_ansi_code "\u001b[4m"
-    set -l reversed_ansi_code "\u001b[7m"
-    set -l reset_ansi_code "\u001b[0m"
-    set -l teal_ansi_code "\u001b[38;5;31m"
-    set -l green_ansi_code "\u001b[38;5;70m"
-    set -l gold_ansi_code "\u001b[38;5;220m"
+    set --local --export MANPATH $MANPATH
 
-    set -x LESS_TERMCAP_md (printf $bold_ansi_code$teal_ansi_code) # start bold
-    set -x LESS_TERMCAP_me (printf $reset_ansi_code) # end bold
-    set -x LESS_TERMCAP_us (printf $underline_ansi_code$green_ansi_code) # start underline
-    set -x LESS_TERMCAP_ue (printf $reset_ansi_code) # end underline
-    set -x LESS_TERMCAP_so (printf $reversed_ansi_code$gold_ansi_code) # start standout
-    set -x LESS_TERMCAP_se (printf $reset_ansi_code) # end standout
-    command man $argv
+    # special case for NetBSD and FreeBSD: set MANPATH if not already set
+    # see https://github.com/fish-shell/fish-shell/blob/555af37616893160ad1afb208a957d6a01a7a315/share/functions/man.fish#L15
+    if test -z "$MANPATH"
+        if set path (command man -p 2>/dev/null)
+            set MANPATH (string replace --regex '[^/]+$' '' $path)
+        else
+            set MANPATH ""
+        end
+    end
+
+    # prepend the directory of fish manpages to MANPATH
+    set fish_manpath $__fish_data_dir/man
+    if test -d $fish_manpath
+        set --prepend MANPATH $fish_manpath
+    end
+
+    cless (command --search man) $argv
 end
