fc203e0eb1a1bffd8fbdd07f5b6af0cfe77ce4e4
Christian Romney <christian.romney@cognitect.com>
2022-06-01 17:42:59 -0400

    Formatting
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- doom/dot-doom.d/+custom-functions.el
+++ doom/dot-doom.d/+custom-functions.el
@@ -121,14 +121,16 @@ returns the conversion of each to the other. "
        (celsius . ,celsius))))
 
 (defun message-temperature-conversions (num)
-  "Interprets the given num as farenheit and celsius degrees and displays the conversions in the echo area."
+  "Interprets the given num as farenheit and celsius degrees and
+displays the conversions in the echo area."
   (let* ((temps (temperature-conversions num))
          (degf  (alist-get 'farenheit temps))
          (degc  (alist-get 'celsius temps)))
     (message "Temperatures: %2.1f℃ => %2.1f℉; %2.1f℉ => %2.1f℃" num degf num degc)))
 
 (defun display-temperature-at-point-conversions ()
-  "Displays the number at point as both farenheit and celsius degrees in the echo area."
+  "Displays the number at point as both farenheit and celsius
+degrees in the echo area."
   (interactive)
   (let ((num (number-at-point)))
     (if num (message-temperature-conversions num))))
