c0a9338e877fefcf28dcbe3b55eafb51bee36d20
Christian Romney <christian.romney@cognitect.com>
2022-08-20 13:27:05 -0400

    Converted emacs configuration to literate org mode

--- doom/dot-doom.d/init.el
+++ doom/dot-doom.d/init.el
@@ -200,5 +200,7 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
+
+(setq +literate-config-file (concat doom-private-dir "readme.org"))
