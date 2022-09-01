a56cd8bc2fc9be2062ae990da6dea6a8203385ae
Christian Romney <christian.romney@cognitect.com>
2022-08-31 22:43:35 -0400

    Change set order for fish vars
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- shell/dot-config/fish/config.fish
+++ shell/dot-config/fish/config.fish
@@ -1,4 +1,6 @@
 set fish_greeting ">>> Welcome back, "(whoami)". <<<"
+set PATH /usr/local/MacGPG2/bin $HOME/bin $HOME/.cargo/bin $HOME/.emacs.d/bin $HOME/.jenv/bin $PATH
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
 
 # ensure gpg-agent is running and add ssh keys quietly
 gpgconf --launch gpg-agent
@@ -11,9 +13,6 @@ set -x GPG_AGENT_INFO "~/.gnupg/S.gpg-agent:"(pgrep gpg-agent)":1"
 if test -f $HOME/.nurc
     bass source $HOME/.nurc
 end
-
-set PATH /usr/local/MacGPG2/bin $HOME/bin $HOME/.cargo/bin $HOME/.emacs.d/bin $HOME/.jenv/bin $PATH
-set -g fish_user_paths /usr/local/sbin $fish_user_paths
 
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
