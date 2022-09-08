76023460ed1479ef03a1a14074db40d095443921
Christian Romney <christian.romney@cognitect.com>
2022-09-08 13:47:55 -0400

    Add mail to Doom dashboard
    Signed-off-by: Christian Romney <christian.romney@cognitect.com>

--- doom/dot-doom.d/config.el
+++ doom/dot-doom.d/config.el
@@ -4,7 +4,6 @@
 (use-package ef-themes
   :init
   (setq doom-font                 "JetBrains Mono:pixelsize=20"
-        doom-unicode-font         "FontAwesome:pixelsize=20"
         display-line-numbers-type t
         fancy-splash-image        (concat doom-private-dir "splash.png"))
 
@@ -17,6 +16,40 @@
   ;; ligature support
   (mac-auto-operator-composition-mode)
   (add-to-list 'default-frame-alist '(fullscreen . maximized)))
+
+(setq +doom-dashboard-menu-sections
+      '(("Reload last session"
+         :icon (all-the-icons-octicon "history" :face 'doom-dashboard-menu-title)
+         :when (cond ((modulep! :ui workspaces)
+                      (file-exists-p (expand-file-name persp-auto-save-fname persp-save-dir)))
+                     ((require 'desktop nil t)
+                      (file-exists-p (desktop-full-file-name))))
+         :face (:inherit (doom-dashboard-menu-title bold))
+         :action doom/quickload-session)
+        ("Open org-agenda"
+         :icon (all-the-icons-octicon "calendar" :face 'doom-dashboard-menu-title)
+         :when (fboundp 'org-agenda)
+         :action org-agenda)
+        ("Open mail"
+         :icon (all-the-icons-faicon "inbox" :face 'doom-dashboard-menu-title)
+         :when (modulep! :ui doom-dashboard)
+         :action =mu4e)
+        ("Recently opened files"
+         :icon (all-the-icons-octicon "file-text" :face 'doom-dashboard-menu-title)
+         :action recentf-open-files)
+        ("Open project"
+         :icon (all-the-icons-octicon "briefcase" :face 'doom-dashboard-menu-title)
+         :action projectile-switch-project)
+        ("Jump to bookmark"
+         :icon (all-the-icons-octicon "bookmark" :face 'doom-dashboard-menu-title)
+         :action bookmark-jump)
+        ("Open private configuration"
+         :icon (all-the-icons-octicon "tools" :face 'doom-dashboard-menu-title)
+         :when (file-directory-p doom-user-dir)
+         :action doom/open-private-config)
+        ("Open documentation"
+         :icon (all-the-icons-octicon "book" :face 'doom-dashboard-menu-title)
+         :action doom/help)))
 
 (defun +mkdirp (path)
   "Ensures the directory path exists, creating any parents as
